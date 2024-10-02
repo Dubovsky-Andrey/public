1586a0de9575b5859f5cbd5eadad9c7d8d7a8fb6
Andrey <dubovsky.andrey.work@gmail.com>
2024-10-02 11:04:09 -0400

    add known host stategy

--- jenkins-setup-agents/linux/ubuntu/ubuntu_24_04_add_ssh_keys.sh
+++ jenkins-setup-agents/linux/ubuntu/ubuntu_24_04_add_ssh_keys.sh
@@ -14,20 +14,34 @@
 # Developed by Andrey Dubovsky
 #--------------------------------------------------------------------
 
+LOG_FILE="/var/log/jenkins_ssh_sudo_setup.log"
+
 # Function for logging info
 function log_info() {
     echo "[INFO] $1"
+    echo "[INFO] $1" >> "$LOG_FILE"
 }
 
 # Function for logging success
 function log_success() {
     echo "[SUCCESS] $1"
+    echo "[SUCCESS] $1" >> "$LOG_FILE"
 }
 
 # Function for logging errors
 function log_error() {
     echo "[ERROR] $1"
+    echo "[ERROR] $1" >> "$LOG_FILE"
     exit 1
+}
+
+# Function for checking command success
+function check_command_success() {
+    if [ $? -ne 0 ]; then
+        log_error "$1"
+    else
+        log_success "$2"
+    fi
 }
 
 # Check and create .ssh directory if it doesn't exist
@@ -37,12 +51,12 @@ function check_ssh_dir() {
         mkdir /home/jenkins/.ssh
         chmod 700 /home/jenkins/.ssh
         chown jenkins:jenkins /home/jenkins/.ssh
-        log_success "SSH directory created with correct permissions."
+        check_command_success "Failed to create .ssh directory" "SSH directory created with correct permissions."
     else
         log_info "SSH directory exists. Checking permissions..."
         chmod 700 /home/jenkins/.ssh
         chown jenkins:jenkins /home/jenkins/.ssh
-        log_success "Permissions on SSH directory updated."
+        check_command_success "Failed to update permissions on .ssh directory" "Permissions on SSH directory updated."
     fi
 }
 
@@ -53,35 +67,37 @@ function check_authorized_keys() {
         touch /home/jenkins/.ssh/authorized_keys
         chmod 600 /home/jenkins/.ssh/authorized_keys
         chown jenkins:jenkins /home/jenkins/.ssh/authorized_keys
-        log_success "authorized_keys file created with correct permissions."
+        check_command_success "Failed to create authorized_keys file" "authorized_keys file created with correct permissions."
     else
         log_info "authorized_keys file exists. Checking permissions..."
         chmod 600 /home/jenkins/.ssh/authorized_keys
         chown jenkins:jenkins /home/jenkins/.ssh/authorized_keys
-        log_success "Permissions on authorized_keys updated."
+        check_command_success "Failed to update permissions on authorized_keys" "Permissions on authorized_keys updated."
     fi
 }
 
 # Ask if user wants to add SSH key
 function add_ssh_key() {
-
     read -p "Please enter the public SSH key: " ssh_key
     echo "$ssh_key" >> /home/jenkins/.ssh/authorized_keys
     chmod 600 /home/jenkins/.ssh/authorized_keys
     chown jenkins:jenkins /home/jenkins/.ssh/authorized_keys
-    log_success "SSH key added to authorized_keys."
-
+    check_command_success "Failed to add SSH key to authorized_keys" "SSH key added to authorized_keys."
 }
 
 # Add Jenkins to sudo group
 function add_to_sudo() {
-
     sudo usermod -aG sudo jenkins
-    log_success "Jenkins user added to sudo group."
-
+    check_command_success "Failed to add Jenkins user to sudo group" "Jenkins user added to sudo group."
+    
     echo "jenkins ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/jenkins > /dev/null
-    log_success "Configured sudo without password for Jenkins user."
+    check_command_success "Failed to configure sudo without password for Jenkins user" "Configured sudo without password for Jenkins user."
+}
 
+# Add host verification strategy to github
+function add_known_host_github() {
+    sudo ssh-keyscan -t rsa github.com >> /home/jenkins/.ssh/known_hosts
+    check_command_success "Failed to add verification github strategy" "Added keys to /home/jenkins/.ssh/known_hosts"
 }
 
 # Main function to run the checks and configurations
@@ -92,9 +108,12 @@ function main() {
     check_authorized_keys
     add_ssh_key
     add_to_sudo
-
+    add_known_host_github
     log_success "SSH and sudo configuration completed."
 }
+
+
+
 
 # Run the main function
 main
